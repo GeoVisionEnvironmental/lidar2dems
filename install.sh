e03a49df2a4ae2340e6cb8713919389d175a6438
Piero Toffanin <pt@masseranolabs.com>
2017-06-29 10:38:37 -0400

    Added prefix in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-CPLUS_INCLUDE_PATH=/usr/include/gdal C_INCLUDE_PATH=/usr/include/gdal ./setup.py install
-if [ ! -f /usr/bin/pdal ]; then
-	ln -vs /code/SuperBuild/build/pdal/bin/pdal /usr/bin/pdal
-fi
+CPLUS_INCLUDE_PATH=/usr/include/gdal C_INCLUDE_PATH=/usr/include/gdal ./setup.py install --prefix="$1"
