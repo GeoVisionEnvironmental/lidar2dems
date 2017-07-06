b5f8e90eb977294fed84c5ae7d85c37de1fd39c6
Piero Toffanin <pt@masseranolabs.com>
2017-07-06 14:40:31 -0400

    Added check for dist-packages directory creation

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+python_dist_dir="$1/lib/python2.7/dist-packages/"
+if [ ! -e $python_dist_dir ]; then
+	echo "Python dist-packages dir does not exist. Creating it now..."
+	mkdir -p $python_dist_dir
+fi
 CPLUS_INCLUDE_PATH=/usr/include/gdal C_INCLUDE_PATH=/usr/include/gdal ./setup.py install --prefix="$1"  --install-layout deb
